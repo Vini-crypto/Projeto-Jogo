24b8313e7af76ec153a9464805929c319c8ab6cb
Vini-crypto <66035784+Vini-crypto@users.noreply.github.com>
2020-05-27 18:22:53 -0300

    Add files via upload

---
+++ Scripts/Texto1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Texto1 : MonoBehaviour
+{
+	public GameObject numeros;
+	public Text texto;
+	private GameObject conta;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		texto.enabled = false;
+		conta = GameObject.FindWithTag("Player");
+	}
+
+// Update is called once per frame
+	void Update()
+	{
+		if (Vector3.Distance(transform.position, conta.transform.position) < 2)
+		{
+			texto.enabled = true;
+		}
+		else
+		{
+			texto.enabled = false;
+		}
+	}
+
+
+}	
